bf627505b4b7bee0f89eac7cc5655c6b25f59ff4
jsy <1729135661@qq.com>
2025-01-05 15:51:33 +0800

    change logger, generate log file by date

--- rpc/include/rpc/Logger.hpp
+++ rpc/include/rpc/Logger.hpp
@@ -1,15 +1,19 @@
 #include <memory>
 #include <spdlog/logger.h>
-#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/daily_file_sink.h>
 #include <spdlog/spdlog.h>
 
 class Logger
 {
 public:
-    static void init(const std::string &logFile = "/home/jsy/learn/myrpc/log/default.log");
+    // 初始化日志器
+    static void init(const std::string &logFileDir = "/home/jsy/learn/myrpc/log/");
 
+    // 获取日志器
     static std::shared_ptr<spdlog::logger> getLogger();
 
 private:
-    inline static std::shared_ptr<spdlog::logger> logger_ = nullptr;
+    inline static std::shared_ptr<spdlog::logger> logger_ = nullptr;                  // 日志器
+    inline static std::shared_ptr<spdlog::details::thread_pool> thread_pool_ = nullptr; // 异步线程池
 };
--- rpc/src/Logger.cpp
+++ rpc/src/Logger.cpp
@@ -1,17 +1,35 @@
 #include <rpc/Logger.hpp>
-#include <spdlog/common.h>
-#include <spdlog/sinks/basic_file_sink.h>
-#include <spdlog/spdlog.h>
 
-void Logger::init(const std::string &logFile)
+void Logger::init(const std::string &logFileDir)
 {
     if (!logger_)
     {
-        logger_ = spdlog::basic_logger_mt("Logger", logFile);
+        // 创建异步线程池
+        size_t queue_size = 8192; // 队列大小
+        thread_pool_ = std::make_shared<spdlog::details::thread_pool>(queue_size, 1);
+
+        // 创建按日期分割的文件 sink
+        auto daily_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(
+            logFileDir + "log.log", 0, 0); // 每天 00:00 创建新文件
+
+        // 创建异步日志器
+        logger_ = std::make_shared<spdlog::async_logger>(
+            "Logger",
+            spdlog::sinks_init_list{daily_sink}, // 使用按日期分割的 sink
+            thread_pool_,
+            spdlog::async_overflow_policy::block);
+
+        // 设置默认日志器
         spdlog::set_default_logger(logger_);
-        spdlog::flush_on(spdlog::level::debug);
+
+        // 设置日志刷新策略
+        spdlog::flush_on(spdlog::level::info);
+
+        // 设置日志输出级别
         spdlog::set_level(spdlog::level::debug);
-        logger_->info("Logger initialized with file: {}", logFile);
+
+        // 初始化完成日志消息
+        logger_->info("Logger initialized with directory: {}", logFileDir);
     }
 }
 
